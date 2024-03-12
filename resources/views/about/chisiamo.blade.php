e7ae92a538e140b9e320bae47c900c5e0f3c3d3e
MyUsername <figusgiacomo04@gmail.com>
2024-03-12 18:44:15 +0100

    Exercise completed

---
+++ resources/views/about/chisiamo.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- TAILWIND CSS --}}
+    @vite('resources/css/app.css')
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @include('components.header')
+    <h1 class="text-3xl">Ciao Mondo 2!</h1>
+</body>
+</html>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,15 @@
+
+    <header class="bg-gray-800 text-white flex justify-between items-center px-4 py-3">
+        <div class="flex items-center">
+            <img src="logo.png" alt="Logo" class="h-8 mr-2">
+            <h1 class="text-lg font-semibold">Il tuo sito</h1>
+        </div>
+        <nav>
+            <ul class="flex space-x-4">
+                <li><a href="#" class="hover:text-gray-300">Home</a></li>
+                <li><a href="#" class="hover:text-gray-300">Servizi</a></li>
+                <li><a href="{{ route('chi-siamo') }}" class="hover:text-gray-300">Chi siamo</a></li>
+                <li><a href="#" class="hover:text-gray-300">Contatti</a></li>
+            </ul>
+        </nav>
+    </header>
